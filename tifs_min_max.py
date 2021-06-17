c03907717938e07ce985bf2a9733910e0e470f16
lyesds <lroubah@gmail.com>
2021-06-17 12:31:21 +0200

    save chart, create df of corners and locate x,y

--- tifs_min_max.py
+++ tifs_min_max.py
@@ -25,22 +25,39 @@ for filename in os.listdir(directory):
         cx = (ulx + urx)/2  # center X
         cy = (uly + lly)/2  # upper Y
 
-        # print(ulx, uly)
-        # print(llx, lly)
-        # print(urx, ury)
-        # print(lrx, lry)
         plt.plot([ulx, urx, lrx, llx, ulx], [uly, ury, lry, lly, uly], label=filename[-6:-4])
         plt.text(llx, lly, filename[-6:-4], color='b')
 
-        # data = [[filename[-6:-4]], [ulx], [urx], [lrx], [llx], [uly], [ury], [lry], [lly]]
-        # data = list(zip(filename[-6:-4], ulx, urx, lrx, llx, uly, ury, lry, lly))
-        # df_corners = pd.DataFrame(data, columns=['id','ulx','urx','lrx','llx','uly','ury','lry','lly'])
-        # df_corners = pd.Series(data)
+        data = [filename[-6:-4], ulx, urx, lrx, llx, uly, ury, lry, lly]
+        df_corners.loc[len(df_corners)] = data
     else:
         continue
 plt.axis([0, 3e5, 1.25e5, 2.75e5])
 plt.title('DSM GeoTiff files of Flanders in Belgium (Lambert72 coordinates)')
 # plt.legend()
-plt.show()
+# plt.show()
+plt.savefig("data/DSM/GeoTIFF/DSM_tif_files.png", transparent=True)
 
-print(df_corners.head(20))
+# print(df_corners.head(20))
+# print(df_corners.info())
+
+
+def locate(x: float, y: float) -> list:
+    ls = []
+    for i in range(df_corners.shape[0]):
+        if x >= df_corners.iloc[i, 1] and x <= df_corners.iloc[i, 2]:
+            ls.append(df_corners.iloc[i, 0])
+        else:
+            continue
+    df_corners_sub = df_corners[df_corners['id'].isin(ls)]
+    ls = []
+    for j in range(df_corners_sub.shape[0]):
+        if y >= df_corners_sub.iloc[j, 8] and y <= df_corners_sub.iloc[j, 5]:
+            ls.append(df_corners_sub.iloc[j, 0])
+        else:
+            continue
+    return ls
+
+
+print(locate(x=152458.45, y=212084.91))  # Schoenmarkt 35, 2000 Antwerpen
+print(locate(x=150000, y=140000))  # should return empty list because outside Flanders
