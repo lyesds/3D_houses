e3c6f9cc589fb0a9af0d3a19659c34355ff176f6
Durieux <durieux.arnaud@hotmail.com>
2021-06-24 10:35:36 +0200

    debugged the problem with the polygon

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from utils.rendering import render_bbox, render_plot_around_xy, render_polygon
 
 x, y = lambert_x_y("Kolkstraat 35, 9100 Sint-Niklaas")
 # x, y = lambert_x_y("Groenplaats 21, 2000 Antwerpen")
-# x, y = lambert_x_y("Schoenmarkt 35, 2000 Antwerpen")
+x, y = lambert_x_y("Schoenmarkt 35, 2000 Antwerpen")
 
 
 folder_path = "/media/arnaud/0AE494D0E494BEFF/3d-house-data/Cadastral_data"
@@ -22,7 +22,4 @@ for filename in myFilesPaths:
             geom = gdf["geometry"][0]
 
 geotiff_path = "/media/arnaud/0AE494D0E494BEFF/3d-house-data/DHMVIIDSMRAS1m_k15.tif"
-# # tif_number = locate("/media/arnaud/0AE494D0E494BEFF/3d-house-data/", x, y)[0]
-render_plot_around_xy("/media/arnaud/0AE494D0E494BEFF/3d-house-data/", x, y)
-render_bbox([(x - 40, y - 40), (x + 40, y + 40)], geotiff_path)
 render_polygon(geom, geotiff_path)
